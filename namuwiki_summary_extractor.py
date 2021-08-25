a8ff8c1ed05e3f4c98a0214f908f0505d5a77daf
chisacam <dladsds123@gmail.com>
2021-08-25 22:10:26 +0900

    init commit

---
+++ namuwiki_summary_extractor.py
@@ -0,0 +1,54 @@
+import ijson
+from namuwiki.extractor import extract_text
+import re
+import json
+import datetime
+import argparse
+
+def get_argparser():
+    today_string = datetime.datetime.strftime(datetime.datetime.now(), '%Y%m%d')
+    outputfile_path = f'./result_{today_string}.json'
+    inputfile_path = './input.json'
+    parser = argparse.ArgumentParser(description='namu_summary_extracter')
+    parser.add_argument('--dump_path', type=str, required=False,
+                        default=inputfile_path)
+    parser.add_argument('--output_file', type=str, required=False,
+                        default= outputfile_path)
+    return parser
+
+if __name__ == '__main__':
+    result = []
+    section_regex = re.compile(r'==[ 가-힣]+==')
+    parser = get_argparser()
+    args = parser.parse_args()
+
+    with open(args.dump_path, 'r') as namu_dirty:
+        parser = ijson.items(namu_dirty, 'item')
+        for dirty_object in parser:
+            sections = section_regex.findall(dirty_object['text'])
+            try:
+                index = sections.index('== 개요 ==')
+                paragraphs = section_regex.split(dirty_object['text'])
+                clean_text = extract_text(paragraphs[index+1])
+                print(dirty_object)
+                print(clean_text)
+                if clean_text != '':
+                    result.append({
+                        'title': dirty_object['title'],
+                        'text': clean_text
+                    })
+                    break
+            except:
+                if len(sections) < 1:
+                    paragraphs = section_regex.split(dirty_object['text'])
+                    clean_text = extract_text(paragraphs[0])
+                    if clean_text != '':
+                        result.append({
+                            'title': dirty_object['title'],
+                            'text': clean_text
+                        })
+                continue
+        print('작업끝!')
+
+    with open(args.output_file, 'w') as clean_json:
+        json.dump(result, clean_json, indent=4, ensure_ascii=False)
